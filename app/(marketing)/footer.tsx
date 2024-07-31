10de0ddba5dac4f77d4aefb64e495b4edd64f263
Sidhanbir <aadesh0313@gmail.com>
2024-07-31 11:35:25 +0530

    Minor

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-
-import { Button } from "@/components/ui/button";
-
 export const Footer = () => {
   return (
     <div className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
-      <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
-        
+      <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">     
     </div></div>
   );
 };
