a8c80bec142a24b02a048d172314ee10a46ae252
Sidhanbir <aadesh0313@gmail.com>
2024-07-31 12:04:55 +0530

    Minor

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,9 +1,9 @@
 
 import Image from "next/image";
-
+import Link from "next/link";
 import { Progress } from "@/components/ui/progress";
-
-
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
 type HeaderProps = {
   hearts: number;
   percentage: number;
@@ -18,8 +18,11 @@ export const Header = ({
 
   return (
     <header className="mx-auto flex w-full max-w-[1140px] items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
-     
-
+      <Link href="/learn">
+        <Button size="sm" variant="ghost">
+          <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
+        </Button>
+      </Link>
       <Progress value={percentage} />
 
       <div className="flex items-center font-bold text-rose-500">
